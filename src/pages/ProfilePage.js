7c5e926de87bd00afa9d071676b0ac07873f8267
shankaplier <99546364+shankaplier@users.noreply.github.com>
2026-03-24 11:04:00 -0400

    Updated the Frontend of the ProfilePage
    Updated the profile page to be consistent with the Figma Design

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -3,6 +3,9 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useUser } from "@clerk/react";
+import Profile from '../assets/profile.jpg';
+import VerifiedBadge from '../assets/verified-badge.png';
+import Star from '../assets/Star.png';
 
 // Page to display user profiles
 export function ProfilePage() {
@@ -59,11 +62,59 @@ export function ProfilePage() {
     };
     itemFetch();
   }, [id]);
+
+
+  const [orderState, setOrderState] = useState("past");
+
   return (
     <main>
-      <h1>User Profile</h1>
-      {info
-      ? (
+      {/* Profile picture and name with verified badge and rating */}
+      <img src={Profile} alt="An anime character with a shocked face" class="rounded-full w-32 h-32"/>
+      <span class="flex mt-2">
+      <h1 class="text-3xl font-bold mt-0.5">Shashank Gutta</h1>
+      <img src={VerifiedBadge} alt="Verified badge" class="mt-0.5 ml-1 w-10 h-10"/>
+      <h1 class="text-3xl font-bold mt-0.5 ml-1">4.9</h1>
+      <img src={Star} alt="Star" class="mt-[6px] ml-1 w-7 h-7"/>
+      </span>
+
+      {/* Listing Stats */}
+      <div class="flex">
+      <p>5 active listings</p>
+      <p class="ml-9">30 total listings</p>
+      </div>
+      
+      
+      <div class="h-screen max-h-[500px] bg-[#0033A0]/60 mt-4 rounded-3xl">
+      <div class="flex flex-row justify-between">
+        <span class='flex flex-row'>
+          {/* {orderState === "active" ? "bg-gray-500" : "bg-[#0033A0]/60"} */}
+        <div class={`cursor-pointer ${orderState === "active" ? "bg-gray-500  rounded-tl-2xl" : ""}`} onClick={() => setOrderState("active")}>
+          <div class="text-white text-2xl m-5 ml-5">Active Orders</div>
+        </div>
+        <div class={`cursor-pointer ${orderState === "past" ? "bg-gray-500" : ""}`} onClick={() => setOrderState("past")}>
+          <div class="text-white text-2xl m-5 ml-5 pl-5 pr-5">Past Orders</div>
+        </div>
+        </span>
+        <span class="flex flex-row">
+           <div onClick={() => {console.log("Buyer clicked")}} class="p-5 hover:bg-gatorOrange mr-3 cursor-pointer">
+            <div class="text-white text-2xl">Buyer</div>
+          </div>
+          <div onClick={() => {console.log("Seller clicked")}} class="p-5 hover:bg-gatorOrange rounded-tr-2xl -ml-3 cursor-pointer">
+            <div class="text-white text-2xl">Seller</div>
+          </div>
+        </span>
+
+      </div>
+      {orderState === "active"||"past" ? 
+        (<div class="bg-gray-500 h-full rounded-b-3xl">
+
+        </div>) 
+        : (<p>Loading orders...</p>)}
+      </div>
+
+
+
+      {/* {info ? (
         <div>
           <p>{info.profile.profileName}</p>
           <img src={info.profile.profilePicture}/>
@@ -113,7 +164,7 @@ export function ProfilePage() {
           : <></>}
         </div>)
       : (!error && <p>Loading profile</p>)}
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>} */}
     </main>
   );
 }
